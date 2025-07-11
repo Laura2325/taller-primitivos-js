65f163bcb30fe3a2620f71083d13890b03f80194
Laura2325 <valensanchez2325@email.com>
2025-07-10 23:08:06 -0500

    Agregué los ejercicios 1 y 2 de variables

---
+++ app.js
@@ -0,0 +1,13 @@
+// Ejercicio 1 - Variables y tipos primitivos
+let nombre = "Valentina";
+let edad = 23;
+let esEstudiante = true;
+
+console.log(nombre, edad, esEstudiante);
+
+// Ejercicio 2
+let sinValor;
+let valorNulo = null;
+
+console.log("sinValor:", sinValor);
+console.log("valorNulo:", valorNulo);
