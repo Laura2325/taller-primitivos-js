f46e9558138d0be1ad0b8150eb8e45333ad7c2b2
Laura2325 <valensanchez2325@gmail.com>
2025-07-13 22:11:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,3 @@ let edad = 23;
 let esEstudiante = true;
 
 console.log(nombre, edad, esEstudiante);
-
-// Ejercicio 2
-let sinValor;
-let valorNulo = null;
-
-console.log("sinValor:", sinValor);
-console.log("valorNulo:", valorNulo);
